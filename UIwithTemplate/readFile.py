fc47b37023978799053731dddfa75a8fb89ead72
MoSangIl_mac <mosang1230@gmail.com>
2021-05-14 14:46:39 +0900

    CREATE imagination algorithm for tsdata

--- UIwithTemplate/readFile.py
+++ UIwithTemplate/readFile.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 from itertools import product
-import copy
-from pyts.image import RecurrencePlot
+
 
 def split_into_values(df, columns):
     # Columns 사용 Parameter 종류 추출
@@ -10,7 +9,7 @@ def split_into_values(df, columns):
     for param in columns:
         df_params = df.loc[:, [param]].drop_duplicates().reset_index().drop(columns='index').transpose()
         params_list.append(df_params.values[0].tolist())
-        
+
     # Parameter Columns 가 가질 수 있는 모든 조합 생성
     parameter_combination_sets = list(product(*params_list))
 
@@ -20,22 +19,8 @@ def split_into_values(df, columns):
         for i, param in enumerate(param_set):
             # Parameter 각 조합을 반복문을 통해 파싱한다.
             part_df = part_df.loc[part_df[columns[i]] == param, :]
-        # 값들만 결과에 저장    
+        # 값들만 결과에 저장
         part_df = part_df.reset_index().drop(columns=['index']+columns).transpose()
         result_df = result_df.append(part_df, ignore_index=True)
-    # 반환    
+    # 반환
     return result_df[pd.notnull(result_df[0])]
-
-def toRPdata(tsdatas, dimension=1, time_delay=1, threshold=None, percentage=10, flatten=False):
-    X = []
-    rp = RecurrencePlot(dimension= dimension,
-                        time_delay= time_delay,
-                        threshold= threshold,
-                        percentage= percentage,
-                        flatten= flatten)
-    for data in tsdatas:
-        data_rp = rp.fit_transform(data)
-        X.append(data_rp[0])
-    X = np.array(X)
-    return X
-    
