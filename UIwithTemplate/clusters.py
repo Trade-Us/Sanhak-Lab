0fc13e3e8159a757958fd24cba2b6804def69075
MoSangIl <mosang1230@naver.com>
2021-05-17 23:15:11 +0900

    REARRANGE files of UI, Delete previous files & Create new files

--- UIwithTemplate/clusters.py
+++ UIwithTemplate/clusters.py
@@ -1,16 +1,27 @@
 import dash_html_components as html
 
-from algorithms import *
+from cluster_layout import *
+from dim_rduction_layout import *
+from img_layout import *
+from normalize_layout import *
 
 def ts_sample_kmeans():
     return html.Div(id='ts-sample-kmeans', children=[
         html.Div(id='hidden-ts-sample-kmeans', style={'display':'none'}),
+        timeseries_resampler_layout(),
         kmeans_layout()
     ])
 def ts_sample_hierarchy():
     return html.Div(id='ts-sample-hierarchy', children=[
         html.Div(id='hidden-ts-sample-hierarchy', style={'display':'none'}),
+        timeseries_resampler_layout(),
         hierarchy_layout()
+    ])
+def ts_sample_ts_kmeans():
+    return html.Div(id='ts-sample-ts-kmeans', children=[
+        html.Div(id='hidden-ts-sample-ts-kmeans', style={'display':'none'}),
+        timeseries_resampler_layout(),
+        time_sereies_kmeans_layout()
     ])
 def rp_ae_kmeans():
     return html.Div(id='rp-ae-kmeans', children=[
@@ -31,5 +42,68 @@ def rp_ae_dbscan():
         html.Div(id='hidden-rp-ae-dbscan', style={'display':'none'}),
         rp_layout(),
         autoencoder_layout(),
+        dbscan_layout()
+    ])
+
+def gaf_ae_kmeans():
+    return html.Div(id='gaf-ae-kmeans', children=[
+        html.Div(id='hidden-gaf-ae-kmeans', style={'display':'none'}),
+        gaf_layout(),
+        autoencoder_layout(),
+        kmeans_layout(),
+    ])
+def gaf_ae_hierarchy():
+    return html.Div(id='gaf-ae-hierarchy', children=[
+        html.Div(id='hidden-gaf-ae-hierarchy', style={'display':'none'}),
+        gaf_layout(),
+        autoencoder_layout(),
+        hierarchy_layout()
+    ])
+def gaf_ae_dbscan():
+    return html.Div(id='gaf-ae-dbscan', children=[
+        html.Div(id='hidden-gaf-ae-dbscan', style={'display':'none'}),
+        gaf_layout(),
+        autoencoder_layout(),
+        dbscan_layout()
+    ])
+
+def mtf_ae_kmeans():
+    return html.Div(id='mtf-ae-kmeans', children=[
+        html.Div(id='hidden-mtf-ae-kmeans', style={'display':'none'}),
+        mtf_layout(),
+        autoencoder_layout(),
+        kmeans_layout(),
+    ])
+def mtf_ae_hierarchy():
+    return html.Div(id='mtf-ae-hierarchy', children=[
+        html.Div(id='hidden-mtf-ae-hierarchy', style={'display':'none'}),
+        mtf_layout(),
+        autoencoder_layout(),
+        hierarchy_layout()
+    ])
+def mtf_ae_dbscan():
+    return html.Div(id='mtf-ae-dbscan', children=[
+        html.Div(id='hidden-mtf-ae-dbscan', style={'display':'none'}),
+        mtf_layout(),
+        autoencoder_layout(),
+        dbscan_layout()
+    ])
+
+def wavelet_kmeans():
+    return html.Div(id='wavelet-kmeans', children=[
+        html.Div(id='hidden-wavelet-dbscan', style={'display':'none'}),
+        wavelet_layout(),
+        dbscan_layout()
+    ])
+def wavelet_hierarchy():
+    return html.Div(id='wavelet-hierarchy', children=[
+        html.Div(id='hidden-wavelet-dbscan', style={'display':'none'}),
+        wavelet_layout(),
+        dbscan_layout()
+    ])
+def wavelet_dbscan():
+    return html.Div(id='wavelet-dbscan', children=[
+        html.Div(id='hidden-wavelet-dbscan', style={'display':'none'}),
+        wavelet_layout(),
         dbscan_layout()
     ])
--- UIwithTemplate/utils/clustering.py
+++ UIwithTemplate/utils/clustering.py
@@ -1,37 +1,37 @@
 # Clustering Algorithm
 from sklearn.cluster import DBSCAN, KMeans, SpectralClustering, AgglomerativeClustering
-
+from tslearn.clustering import TimeSeriesKMeans
 # Needed Library!
 import pandas as pd
 import numpy as np
 
 # Cluster Algorithm
-def kmeans(dataset, n_clusters, n_init = 10, max_iter = 300, tol = e-4, normalization='standard'):
+def kmeans(dataset, n_clusters, n_init = 10, max_iter = 300, tol = e-4):
 
     cluster_data = KMeans(n_clusters=n_clusters, n_init = n_init, max_iter = max_iter, tol = tol).fit(dataset)
     return cluster_data
 
 
 # DBSCAN
-def dbscan(dataset, eps=0.5, min_samples=5, normalization='standard'):
+def dbscan(dataset, eps=0.5, min_samples=5):
 
     dbscan = DBSCAN(eps=eps, min_samples=min_samples)
     cluster_data = dbscan.fit_predict(dataset)
     return cluster_data
 
 # Spectral Clustering
-def spectralClustering(dataset, n_clusters, n_init = 10, normalization='standard'):
+def spectralClustering(dataset, n_clusters, n_init = 10):
     
     cluster_data = SpectralClustering(n_clusters=n_clusters, n_init=n_init).fit_predict(dataset)
     return cluster_data
 
 # Hierarchical Clustering
-def hierarchicalClustering(dataset, n_clusters, n_init = 10, linkage = 'ward', normalization='standard'):
+def hierarchicalClustering(dataset, n_clusters, n_init = 10, linkage = 'ward'):
 
-    cluster_data = AgglomerativeClustering(n_clusters = n_clusters, linkage = linkage ).fit(dataset)
+    cluster_data = AgglomerativeClustering(n_clusters = n_clusters, linkage = linkage, n_init=n_init ).fit(dataset)
     return cluster_data
 
  
-def ts_kmeans_clustering(dataset, n_clusters, random_state):
-    cluster_data = TimeSeriesKMeans(n_clusters=n_clusters, random_state=random_state).fit(dataset)
+def ts_kmeans_clustering(dataset, n_clusters, n_init=1, metric='euclidean'):
+    cluster_data = TimeSeriesKMeans(n_clusters=n_clusters, n_init=n_init, metric=metric).fit(dataset)
     return cluster_data
